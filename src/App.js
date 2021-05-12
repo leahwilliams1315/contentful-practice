0c1f38d0a1366314794620c4246bcc85507d1ee8
Leah Williams <leahwilliams@Leahs-MacBook-Pro.local>
2021-05-12 16:17:16 -0400

    set up most of contentful tutorial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import useContentful from "./hooks/use-contentful";
+import Person from "./components/personComponent";
+
+const query = `
+query {
+  person(id: "125eK86tadBMSBwdTy5VKV") {
+    name
+    socialGithub
+    socialLinkedin
+  }
+}
+`;
 
 function App() {
+  let {data, errors} = useContentful(query)
+
+
+  if (errors) {
+    return (
+      <span style={{color: 'red'}}>
+        {errors.map(error => error.message).join(",")}
+      </span>
+    )
+  }
+
+  if (!data) {
+    return <span>Loading...</span>
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Person person={data.person}/>
+      {/*{data.person.name}*/}
     </div>
   );
 }
---
+++ src/hooks/use-contentful.js
@@ -0,0 +1,29 @@
+import {useEffect, useState} from "react";
+
+const {REACT_APP_SPACE_ID, REACT_APP_CDA_TOKEN} = process.env;
+
+function useContentful (query){
+  let [data, setData] = useState(null);
+  let [errors, setErrors] = useState(null);
+
+  useEffect(() => {
+      fetch(`https://graphql.contentful.com/content/v1/spaces/${REACT_APP_SPACE_ID}`, {
+        method: "POST",
+        headers: {
+          'Content-type': 'application/json',
+          Authorization: `Bearer ${REACT_APP_CDA_TOKEN}`
+        },
+        body: JSON.stringify({query})
+      }).then(response => response.json()
+        .then(({ data, errors }) => {
+          if(errors) setErrors(errors);
+          if(data) setData(data);
+        }))
+        .catch((error) => setErrors([errors]));
+    }, [query]
+    // without this dependency array, useEffect would run after each render, with it, only runs once, when component is injected in DOM
+  );
+  return {data, errors};
+}
+
+export default useContentful;
